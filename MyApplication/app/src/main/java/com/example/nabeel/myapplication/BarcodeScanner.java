88c71697b19c65e1c62cc050919139069cf25ca0
zportnoff <portnoff@terpmail.umd.edu>
2016-05-02 16:55:28 -0400

    Added Barcode Activity

---
+++ MyApplication/app/src/main/java/com/example/nabeel/myapplication/BarcodeScanner.java
@@ -0,0 +1,13 @@
+package com.example.nabeel.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BarcodeScanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode_scanner);
+    }
+}
