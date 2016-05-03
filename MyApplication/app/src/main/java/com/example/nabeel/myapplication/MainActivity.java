4dabe02b122ded91fba62857d4a4b7a360214d4b
zportnoff <portnoff@terpmail.umd.edu>
2016-05-03 00:49:07 -0400

    Barcode update

--- MyApplication/app/src/main/java/com/example/nabeel/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/nabeel/myapplication/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.nabeel.myapplication;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,23 @@ public class MainActivity extends AppCompatActivity {
     public void launchDemoOverview (View view) {
         Intent intent = new Intent(this, ProductOverview.class);
         startActivity(intent);
+    }
+
+    public void launchBarcodeScanner (View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Intent intent = new Intent(this, ProductOverview.class);
+            startActivity(intent);
+            //Bundle extras = data.getExtras();
+
+        }
     }
 }
