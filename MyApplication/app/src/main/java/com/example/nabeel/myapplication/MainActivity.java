1298ec1fdfa27491d003180fa5c33cc9b76cce76
zportnoff <portnoff@terpmail.umd.edu>
2016-05-03 01:45:10 -0400

    Barcode Scanner update

--- MyApplication/app/src/main/java/com/example/nabeel/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/nabeel/myapplication/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.nabeel.myapplication;
 
 import android.content.Intent;
+import android.hardware.Camera;
+import android.hardware.camera2.*;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
-
+    private Camera mCamera;
+    private CameraView camera ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +27,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchBarcodeScanner (View view) {
+       /* setContentView(R.layout.activity_camera_view);
+        mCamera = Camera.open();
+        camera = new CameraView(getApplicationContext(), mCamera);;
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(preview)*/;
+
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            Toast toast = Toast.makeText(this.getApplicationContext(), "Take a photo of the barcode", Toast.LENGTH_LONG);
+            toast.show();
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
+
     }
 
 
